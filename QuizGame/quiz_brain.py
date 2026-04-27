2addfa034a9f195fe6302ba666970612aad97a13
sabarezaei <saba.rezaee.khavas@gmail.com>
2026-04-26 20:37:54 -0400

    one million dollar painting

--- QuizGame/quiz_brain.py
+++ QuizGame/quiz_brain.py
@@ -2,6 +2,7 @@ class QuizBrain :
     def __init__(self, question_list):
         self.question_number = 0
         self.question_list = question_list
+        self.score = 0
         
         
         
@@ -9,12 +10,18 @@ class QuizBrain :
         current_question = self.question_list[self.question_number]
         self.question_number +=1
         user_answer = input(f'Q.{self.question_number} : {current_question.question} (true/false) ')
+        self.check_answer(user_answer, current_question.answer)
         
         
     def more_questions (self):
-        if self.question_number < len(self.question_list):
-            return True
+        return self.question_number < len(self.question_list)
+
+    
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            print("you got it right")
+            self.score +=1
         else:
-            return False
+            print("you got it wrong")
         
-        
+        print(f"your current score is {self.score} / {self.question_number}")
